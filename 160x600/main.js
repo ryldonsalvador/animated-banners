eef0fa664ccac1a9db11744c3ce532bba16e47b5
ryldonsalvador <50676729+ryldonsalvador@users.noreply.github.com>
2020-02-03 21:23:13 +0800

    Add files via upload
    Upload files

---
+++ 160x600/main.js
@@ -0,0 +1,63 @@
+//JQuery alias
+var $ = function(slt) {
+	return document.querySelector(slt);
+};
+var tl;
+
+function init() {
+    TweenLite.set($('#main_div'),{display:"block"});
+    TweenMax.set($('#plane'),{x:-350,y:530})
+    TweenMax.set($('#sign'),{x:-250})
+    TweenMax.set($('#cta'),{opacity:0,display:"none"})
+    animate();
+}
+
+function animate () {
+    tl = new TimelineLite();
+
+    tl.to($('#sign'),.5,{x:-32,ease: Power2.easeOut})
+    .to($('#cta'),.5,{delay:.5,opacity:1,display:"block",ease: Power2.easeOut})
+    // .to($('#plane'),2,{delay:.5,x:400,y:-500,ease: Power2.easeOut})
+    // .to($('#orangeDiv'),.5,{opacity:1,ease: Power2.easeOut}, "-=1.5")
+    // .to($('#whatareyou'),.5,{opacity:1,ease: Power2.easeOut}, "-=1")
+    // .to($('#img2'),.5,{opacity:1,ease: Power2.easeOut}, "+=1")
+    // .to($('#img2_text'),.5,{opacity:1,ease: Power2.easeOut}, "-=.5")
+    // .to($('#img3'),.5,{opacity:1,ease: Power2.easeOut}, "+=1")
+    // .to($('#img3_text'),.5,{opacity:1,ease: Power2.easeOut}, "-=.5")
+    // .to($('#img4'),.5,{opacity:1,ease: Power2.easeOut}, "+=1")
+    // .to($('#warmculture'),.5,{opacity:1,ease: Power2.easeOut}, "-=.5")
+    // .to($('#warmculture'),.5,{opacity:0,delay:1,ease: Power2.easeOut})
+    // .to($('#logoend'),.5,{opacity:1,ease: Power2.easeOut,onComplete:loopAnim})
+}
+
+function loopAnim(){
+    setTimeout(function(){
+        // tl.restart();
+    },300)
+}
+
+function addListeners(){
+	$('#cta').addEventListener('click', ctaClick);
+    $('#cta').addEventListener('mouseover', ctaOver);
+    $('#cta').addEventListener('mouseout', ctaOut);
+    $('#bgExit').addEventListener('click', bgClick);
+    
+}
+
+function bgClick(){
+	Enabler.exit('Background_Exit');
+}
+
+function ctaClick(){
+	Enabler.exit('CTA_Exit');	
+}
+
+function ctaOver(){
+    TweenLite.to($('#cta'),.5,{opacity:.7});
+}
+
+function ctaOut(){
+    TweenLite.to($('#cta'),.5,{opacity:1});
+}
+
+window.onload = init;
---
+++ 2/160x600/scripts/script.js
@@ -0,0 +1,72 @@
+var adDiv;
+var firstImage;
+var secondImage;
+var staticImage;
+var timeDiv;
+var anim1, anim2, anim3;
+
+function loaded() {
+    console.log('loaded');
+    adDiv = document.getElementById("ad");
+    secondImage = document.getElementById("secondImage");
+    firstImage = document.getElementById("firstImage");
+    staticImage = "http://backend.roundshot.com/cams/249/default";
+    timeDiv = document.getElementById("TimeDiv");
+    anim1 = document.getElementById("anim1");
+    anim2 = document.getElementById("anim2");
+    anim3 = document.getElementById("anim3");
+    loadImage();
+}
+
+function loadImage(){
+    getImage();
+    callTime();
+    startAnim();
+}
+
+function startAnim(){
+  anim.to(anim1,.5,{delay:2,opacity:0, ease: Power3.easeOut},"3")
+  .to(anim2,.5,{delay:2,opacity:1, ease: Power3.easeOut},"3.2")
+  .to(anim2,.5,{delay:4,opacity:0, ease: Power3.easeOut},"6")
+  .to(anim3,.5,{delay:4,opacity:1, ease: Power3.easeOut},"6.2")
+}
+
+function callTime(){
+   var now    = new Date();
+   var hour   = now.getHours();
+   var minute = now.getMinutes();
+   // var second = now.getSeconds();
+   var ap = "am";
+   if (hour   > 11) { ap = "pm";             }
+   if (hour   > 12) { hour = hour - 12;      }
+   if (hour   == 0) { hour = 12;             }
+   if (hour   < 10) { hour   = "0" + hour;   }
+   if (minute < 10) { minute = "0" + minute; }
+   var timeString = hour +':' + minute  +" " + ap;
+    timeDiv.innerHTML = timeString;
+      setTimeout(function(){
+      callTime();
+    },1000)
+}
+function getImage() {
+  var img = new Image();
+  img.onload = function() {
+    firstImage.src = this.src;
+    secondImage.src = this.src;
+    imageSize = this.width;
+    animateBanner();
+  }
+  img.src = staticImage;
+}
+
+function animateBanner() {
+  TweenMax.set(secondImage, {x: imageSize - 1, onComplete: animateBackground});
+    function animateBackground() {
+    TweenMax.to(firstImage, 200, {x: -imageSize, ease: Power0.easeNone});
+    TweenMax.to(secondImage, 200, {x: -1, ease: Power0.easeNone, onComplete: function() {
+    TweenMax.set(firstImage, {x: 0});
+    TweenMax.set(secondImage, {x: imageSize - 1});
+    animateBackground();
+    }});
+  }
+}
